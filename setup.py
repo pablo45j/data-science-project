2d967f5b0f1b237749b15d5891a56728b374d6a9
pablo juarez <pablo45j@gmail.com>
2023-08-03 12:18:07 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projecto para el bootcamp de Codigo Facilito',
+    author='Pablo Juarez',
+    license='MIT',
+)
